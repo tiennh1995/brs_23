954ad42f44fb943b5e93e513086d07603c812785
qtsoul95 <qtsoul95@gmail.com>
2016-06-15 10:17:08 +0700

    Login logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root "static_pages#home"
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
-
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 end
