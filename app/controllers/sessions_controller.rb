67a38e30a1258669db3b1cc6657c06c1fbea477f
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-06-16 10:30:30 +0700

    sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by email: params[:session][:email].downcase
-    if user && user.authenticate params[:session][:password]
+    if user && user.authenticate(params[:session][:password])
       log_in user
-      params[:session][:remember_me] == "1" ? remember user : forget user
+      params[:session][:remember_me] == "1" ? remember(user) : forget(user)
       redirect_to user
     else
-      flash.now[:danger] = "Invalid email/password combinaion"
+      flash.now[:danger] = t "sessions.new.danger"
       render :new
     end
   end
