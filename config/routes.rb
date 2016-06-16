f41494eed8fb05be86d655bb58c1cc78526b2465
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-06-16 11:33:01 +0700

    Sign up 2

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
+  get "signup" => "users#new"
+  resources :users
 end
