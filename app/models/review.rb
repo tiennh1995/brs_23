4509557a936752cb011b0170a4fca3aa19ea791d
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-06-23 11:28:58 +0700

    Edit and delete review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :book
   validates :content, presence: true
-  validates :rated, presence: true
+  validates :rated, presence: true, inclusion: {in: 1..5}
 end
