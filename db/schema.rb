3adadc989963397922cae6f62fd59ffbbcd9cb8d
tiennh1995 <nguyen.huu.tien@framgia.com>
2016-06-28 08:03:00 +0700

    edit and delete comment 2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626032221) do
+ActiveRecord::Schema.define(version: 20160627083410) do
 
   create_table "activities", force: :cascade do |t|
     t.integer  "action_id"
